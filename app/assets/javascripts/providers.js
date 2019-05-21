3c9c6c317aa7fb5d95104275a97e97265fa24b70
malvov <manuel.olivaresbonilla@gmail.com>
2019-05-20 21:20:14 -0600

    maps for provider addres

--- app/assets/javascripts/providers.js
+++ app/assets/javascripts/providers.js
@@ -1,15 +1,68 @@
-function initMap(lat, lng) {
+function initShowMap(lat, lng) {
     var myCoords = new google.maps.LatLng(lat, lng);
     var mapOptions = {
         center: myCoords,
         zoom: 14
     };
 
-    console.log(document.getElementsByClassName('map'));
-    var map = new google.maps.Map(document.getElementsByClassName('map'), mapOptions);
-
+    var map = new google.maps.Map(document.getElementById('show-map'), mapOptions);
+    console.log(mapOptions);
     var marker = new google.maps.Marker({
         position: myCoords,
         map: map
+    });
+};
+
+function initInputMap() {
+    var lat = document.getElementById('provider_address_attributes_latitude').value;
+    var lng = document.getElementById('provider_address_attributes_longitude').value;
+
+    // if not defined create default position
+    if (!lat || !lng){
+        lat = 12.128047;
+        lng = -86.264752;
+        document.getElementById('provider_address_attributes_latitude').value = lat;
+        document.getElementById('provider_address_attributes_longitude').value = lng;
+    }
+
+    var myCoords = new google.maps.LatLng(lat, lng);
+    var mapOptions = {
+        center: myCoords,
+        zoom: 14
+    };
+
+    var map = new google.maps.Map(document.getElementById('input-map'), mapOptions);
+    
+    var marker = new google.maps.Marker({
+        position: myCoords,
+        animation: google.maps.Animation.DROP,
+        map: map,
+        draggable: true
+    });
+
+    function refreshMarker(){
+        var lat = document.getElementById('provider_address_attributes_latitude').value;
+        var lng = document.getElementById('provider_address_attributes_longitude').value;
+        var myCoords = new google.maps.LatLng(lat, lng);
+        marker.setPosition(myCoords);
+        map.setCenter(marker.getPosition());   
+    }
+    
+    // when input values change call refreshMarker
+    document.getElementById('provider_address_attributes_latitude').onchange = refreshMarker;
+    document.getElementById('provider_address_attributes_longitude').onchange = refreshMarker;
+
+    // when marker is dragged update input values
+    marker.addListener('drag', function() {
+        latlng = marker.getPosition();
+        newlat=(Math.round(latlng.lat()*1000000))/1000000;
+        newlng=(Math.round(latlng.lng()*1000000))/1000000;
+        document.getElementById('provider_address_attributes_latitude').value = newlat;
+        document.getElementById('provider_address_attributes_longitude').value = newlng;
+    });
+
+    // When drag ends, center (pan) the map on the marker position
+    marker.addListener('dragend', function() {
+        map.panTo(marker.getPosition());   
     });
 };
