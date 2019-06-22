dbf3e91d72879c75beb0f3b23282273e1e54b3c4
malvov <manuel.olivaresbonilla@gmail.com>
2019-06-21 23:15:37 -0600

    setting up mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,9 +68,22 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'app.precomp.com.ni' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'precomp-ni.herokuapp.com', port: 1025 }
-  config.action_mailer.default_url_options = { host: 'precomp-ni.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default charset: "utf-8"
+  
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: Rails.application.credentials.gmail_user_name,
+      password: Rails.application.credentials.gmail_password,
+      openssl_verify_mode:'none'
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
