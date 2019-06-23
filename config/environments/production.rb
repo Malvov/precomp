390657f89a72ba4624e33e7fd8d8d272e53cd6dd
malvov <manuel.olivaresbonilla@gmail.com>
2019-06-22 20:33:07 -0600

    compiling assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -72,7 +72,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  
+  config.action_mailer.default charset: 'utf-8'
+
   config.action_mailer.smtp_settings = {
       address: "mail.precomp.com.ni",
       port: 26,
