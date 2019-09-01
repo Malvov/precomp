a3b6637e3182f58bac16ebe343617fe16783dcb8
malvov <manuel.olivaresbonilla@gmail.com>
2019-09-01 01:24:17 -0500

    product show almost complete

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,4 +3,12 @@ module ProductsHelper
         return "#{ number_to_currency product.price, locale: 'es-NI' }" if product.currency == 'Córdoba'
         "#{ number_to_currency product.price }"
     end
+
+    def product_image product
+        if product.images.count == 0
+            image_tag 'no-img.jpg', class: 'img-fluid'
+        else
+            image_tag product.images.first, class: 'img-fluid'
+        end
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -20,11 +20,21 @@
 class Product < ApplicationRecord
   belongs_to :provider
   belongs_to :category
+  has_many_attached :images
+
   validates_presence_of :name, :description, :trademark, :currency
   validates_numericality_of :price, greater_than: 0, message: 'is not a number'
 
-  has_many_attached :images
+  scope :related_products_by_category, -> (category_id, provider_id) { 
+    where('category_id = ? and provider_id != ?', category_id, provider_id).order('random()')
+  }
+
+
   
   CURRENCY = ['Dólar', 'Córdoba']
+
+  def related_products_by_provider
+    provider.products.where.not(id: id).order('random()') 
+  end
 
 end
