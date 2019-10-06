d09d5a1465972201d73c152526eccb23e56abc73
malvov <manuel.olivaresbonilla@gmail.com>
2019-10-05 23:43:58 -0500

    styling products index

--- app/controllers/favorite_products_controller.rb
+++ app/controllers/favorite_products_controller.rb
@@ -4,7 +4,7 @@ class FavoriteProductsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def index
-        @favorite_products = current_user.favorite_products.actives.paginate page: params[:page], per_page: 16
+        @favorite_products = current_user.favorite_products.actives.paginate page: params[:page], per_page: 8
     end
 
     def create
