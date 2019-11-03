a55fd355e131aafabf6d83f97d364d7da2b8a5eb
malvov <manuel.olivaresbonilla@gmail.com>
2019-11-02 21:56:17 -0600

    advanced search page

--- app/controllers/advanced_search_controller.rb
+++ app/controllers/advanced_search_controller.rb
@@ -1,4 +1,41 @@
 class AdvancedSearchController < ApplicationController
     def index
+        @results = Array.new
+        @type, @category, @provider = '', '',''
+        if params[:advanced_search]
+            @type = params[:advanced_search][:type]
+            @category = params[:advanced_search][:categories_advanced]
+            @provider = params[:advanced_search][:providers]
+            if @type == 'products'
+                products = Product.actives
+
+                unless @category.blank?
+                    products = products.where category_id: @category
+                end
+
+                unless @provider.blank?
+                    products = products.where provider_id: @provider
+                end
+
+                if !@provider.blank? && !@category.blank?
+                    products = products.where 'provider_id = ? AND category_id = ?', @provider, @category
+                end
+
+                @results = products
+            elsif @type == 'providers'
+                providers = Provider.actives
+
+                unless @provider.blank?
+                    providers = providers.where id: @provider
+                end
+
+                if !@category.blank? && @provider.blank?
+                    providers = providers.joins(:products).where(products: { category_id: @category })
+                end
+
+                @results = providers
+            end
+        end
+        @results
     end
 end
--- app/helpers/providers_helper.rb
+++ app/helpers/providers_helper.rb
@@ -1,2 +1,9 @@
 module ProvidersHelper
+    def provider_image provider
+        unless provider.logo_attachment
+            image_tag 'no-img.jpg', class: 'img-fluid'
+        else
+            image_tag provider.logo, class: 'img-fluid'
+        end
+    end
 end
