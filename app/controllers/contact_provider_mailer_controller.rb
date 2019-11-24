1c7d866afa3053c8da7bf3fdb65c16f9bed3f950
malvov <manuel.olivaresbonilla@gmail.com>
2019-11-23 22:16:33 -0600

    fixed email params

--- app/controllers/contact_provider_mailer_controller.rb
+++ app/controllers/contact_provider_mailer_controller.rb
@@ -4,8 +4,8 @@ class ContactProviderMailerController < ApplicationController
     end
     def create
         @provider = Provider.friendly.find params[:provider_id]
-        @from ||= params[:from]
-        @body ||= params[:body]
+        @from ||= params[:contact_provider_mailer][:from]
+        @body ||= params[:contact_provider_mailer][:body]
         ProviderMailer.with(email: @provider.email, from: @from, body: @body).new.deliver_now
     end
 end
