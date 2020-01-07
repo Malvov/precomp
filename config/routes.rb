c4f385784eb756e2957f6d776a60c7f65551d0c4
malvov <manuel.olivaresbonilla@gmail.com>
2020-01-06 22:06:37 -0600

    index page

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
   resources :favorite_products, only: [:index, :destroy, :create]
   
   root to: 'countdown#index'
+
+  get 'index', to: 'static_pages#index'
   # root to: 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
